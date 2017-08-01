761db8b04f9973ffd86078202271ef6c29b81cea
Jackie Xu <jackie@opakeco.org>
2017-08-02 00:30:30 +0200

    Added initial Karmo crowdsale contract

---
+++ contracts/KarmoCrowdsale.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
+
+/**
+ * @title KarmoCrowdsale
+ */
+contract KarmoCrowdsale is CappedCrowdsale {
+    using SafeMath for uint256;
+
+
+    /**
+     * Returns true if contributors can still contribute to the crowdsale.
+     *
+     * This method overrides the zeppelin capped crowdsale contract to allow
+     * limits on the gas price of a transaction.
+     */
+    function validPurchase() internal constant returns(bool) {
+        require(tx.gasprice == 50);
+
+        return super.validPurchase();
+    }
+}
