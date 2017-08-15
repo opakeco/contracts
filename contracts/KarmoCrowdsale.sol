2b4e779c148dc60a5b7c738d2c6113d35400210a
Jackie Xu <jackie@opakeco.org>
2017-08-16 00:11:24 +0200

    Updated Karmo Crowdsale contract
    - Has a minimum target set; will refund if target is not reached.
    - Allows tokens to be burned.

--- contracts/KarmoCrowdsale.sol
+++ contracts/KarmoCrowdsale.sol
@@ -1,29 +1,47 @@
 pragma solidity ^0.4.11;
 
 import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
+import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
+import 'zeppelin-solidity/contracts/crowdsale/RefundableCrowdsale.sol';
+import 'zeppelin-solidity/contracts/crowdsale/RefundVault.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 
 /**
- * @title KarmoCrowdsale
+ * @title Karmo Crowdsale
  */
-contract KarmoCrowdsale is CappedCrowdsale {
+contract KarmoCrowdsale is CappedCrowdsale, RefundableCrowdsale {
     using SafeMath for uint256;
 
-    uint HARD_LIMIT = 100000;
+    uint256 public MINIMUM_TARGET = 20000 * 1 ether;
+    uint256 public HARD_LIMIT = 100000 * 1 ether;
+    uint256 public EXCHANGE_RATE = 26000;
+    
 
-    function KarmoCrowdsale() {
-        super.CappedCrowdsale(HARD_LIMIT);
+    RefundVault public vault;
+
+    function KarmoCrowdsale(
+        uint256 _startTime,
+        uint256 _endTime,
+        address _wallet
+    )
+        RefundableCrowdsale(MINIMUM_TARGET)
+        CappedCrowdsale(HARD_LIMIT)
+        Crowdsale(_startTime, _endTime, EXCHANGE_RATE, _wallet)
+    {
+        vault = new RefundVault(wallet);
     }
 
     /**
      * Returns true if contributors can still contribute to the crowdsale.
      *
-     * This method overrides the zeppelin capped crowdsale contract to allow
-     * limits on the gas price of a transaction.
+     * This method overrides the zeppelin capped crowdsale contract to allow 
+     * limits on the gas price of a transaction, and to check whether the cap
+     * has been reached.
      */
     function validPurchase() internal constant returns(bool) {
         require(tx.gasprice == 50);
+        require(weiRaised.add(msg.value) <= cap);
 
         return super.validPurchase();
     }
